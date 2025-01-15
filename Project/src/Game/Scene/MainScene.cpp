b42d47cd35f6f38521b5c5564a94c1e2060d7111
안장훈 <wkdgns135@gmail.com>
2025-01-15 20:09:16 +0900

    Update MainScene.cpp

--- Project/src/Game/Scene/MainScene.cpp
+++ Project/src/Game/Scene/MainScene.cpp
@@ -5,6 +5,7 @@ void MainScene::Enter()
 {
 	renderer->AddBackground("drawable/Main.bmp");
 	AddInputEvent(Key_1, []() {SceneManager::GetInstance().ChangeScene<SelectDungeonScene>(); });
+	AddInputEvent(Key_2, []() {SceneManager::GetInstance().ChangeScene<ShopScene>(); });
 	AddInputEvent(Key_ESC, []() {SceneManager::GetInstance().ChangeScene<TitleScene>(); });
 }
 
